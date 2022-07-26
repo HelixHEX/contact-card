97c84acaa42eb83de9c61f99135216d80f158070
Elias <e.wambugu192@gmail.com>
2022-07-25 22:19:00 -0700

    center aligned bio and added creaeted by

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -11,56 +11,63 @@ const App = () => {
         p={{ base: 35, md: 55 }}
         justify="center"
       >
-        <Flex w="100%" flexDir="column">
-          <Image
-            alignSelf={"center"}
-            boxShadow={"2xl"}
-            border={"5px solid white"}
-            w={100}
-            h={100}
-            rounded={"full"}
-            src={require("./assets/images/profile_1.jpg")}
-          />
-          <Text
-            color="gray.50"
-            alignSelf={"center"}
-            fontSize={25}
-            mt={5}
-            fontWeight={"900"}
-          >
-            Elias Wambugu
-          </Text>
-          <Text alignSelf={"center"} color="white" fontWeight={"200"}>
-            Programmer by day. Photographer by night.
-          </Text>
-          <Card
-            link={"gallary.eliaswambugu.com"}
-            color="white"
-            className="gallary"
-          >
-            Gallary
-          </Card>
-          <Card
-            link={"eliaswambugu.com"}
-            color="white"
-            className="projects"
-          >
-            Projects
-          </Card>
-          <Card
-            link={"instagram.com/dandeproductions"}
-            color="white"
-            className="instagram"
-          >
-            Instagram
-          </Card>
-          <Card
-            link={"twitter.com/officialeliasw"}
-            color="white"
-            bg='#1DA1F2'
-          >
-            Twitter
-          </Card>
+        <Flex w="100%" flexDir="column" justify={'space-between'}>
+          <Flex flexDir={'column'}>
+            <Image
+              alignSelf={"center"}
+              boxShadow={"2xl"}
+              border={"5px solid white"}
+              w={100}
+              h={100}
+              rounded={"full"}
+              src={require("./assets/images/profile_1.jpg")}
+            />
+            <Text
+              color="gray.50"
+              alignSelf={"center"}
+              fontSize={25}
+              mt={5}
+              fontWeight={"900"}
+            >
+              Elias Wambugu
+            </Text>
+            <Text
+              textAlign={"center"}
+              alignSelf={"center"}
+              color="white"
+              fontWeight={"200"}
+            >
+              Programmer by day. Photographer by night.
+            </Text>
+            <Card
+              link={"gallary.eliaswambugu.com"}
+              color="white"
+              className="gallary"
+            >
+              Gallary
+            </Card>
+            <Card link={"eliaswambugu.com"} color="white" className="projects">
+              Projects
+            </Card>
+            <Card
+              link={"instagram.com/dandeproductions"}
+              color="white"
+              className="instagram"
+            >
+              Instagram
+            </Card>
+            <Card
+              link={"twitter.com/officialeliasw"}
+              color="white"
+              bg="#1DA1F2"
+            >
+              Twitter
+            </Card>
+          </Flex>
+          <Flex color="white" fontWeight={"200"} alignSelf={"center"}>
+            <Text>Created By: </Text>
+            <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontWeight={'600'}>&nbsp; &nbsp;Elias Wambugu</Text>
+          </Flex>
         </Flex>
       </Flex>
     </>
